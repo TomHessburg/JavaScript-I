d24f0a5e89d3f0c35d480a6a65965397450ad807
Tom Hessburg <tomhessburg@Toms-MacBook-Pro.local>
2019-01-18 18:13:46 -0500

    first part of objects finished

--- assignments/objects.js
+++ assignments/objects.js
@@ -3,17 +3,46 @@
 // ==== Challenge 1: Writing Objects ==== 
 // HR needs some information on the new interns put into a database.  Given an id, email, first name, and gender. Create an object for each person in the company list:
 
-// 1,mmelloy0@psu.edu,Mitzi,F
-// 2,kdiben1@tinypic.com,Kennan,M
-// 3,kmummery2@wikimedia.org,Keven,M
-// 4,gmartinson3@illinois.edu,Gannie,M
-// 5,adaine5@samsung.com,Antonietta,F
+// 1,mmelloy0@psu.edu,Mitzi,F--
+// 2,kdiben1@tinypic.com,Kennan,M--
+// 3,kmummery2@wikimedia.org,Keven,M--
+// 4,gmartinson3@illinois.edu,Gannie,M--
+// 5,adaine5@samsung.com,Antonietta,F--
 
 // Example format of an intern object: 1,examples@you.edu,Example,F
-const example = {
-  "id": 0,
-  "name": "Example",
-  "email": "examples@you.edu",
+
+const obj1 = {
+  "id": 1,
+  "name": "Mitzi",
+  "email": "mmelloy0@psu.edu",
+  "gender": "F"
+}
+
+const obj2 = {
+  "id": 2,
+  "name": "Kennan",
+  "email": "kdiben1@tinypic.com",
+  "gender": "M"
+}
+
+const obj3 = {
+  "id": 3,
+  "name": "Keven",
+  "email": "kmummery2@wikimedia.org",
+  "gender": "M"
+}
+
+const obj4 = {
+  "id": 4,
+  "name": "Gannie",
+  "email": "gmartinson3@illinois.edu",
+  "gender": "M"
+}
+
+const obj5 = {
+  "id": 5,
+  "name": "Antonietta",
+  "email": "adaine5@samsung.com",
   "gender": "F"
 }
 
