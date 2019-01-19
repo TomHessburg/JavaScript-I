a3be5a486205ffd464c3bb1cc42b53dbb4317aae
Tom Hessburg <tomhessburg@Toms-MacBook-Pro.local>
2019-01-18 19:29:36 -0500

    MVP Met

--- assignments/objects.js
+++ assignments/objects.js
@@ -103,13 +103,13 @@ const parent = {
 // Log the parent object's name
   console.log(parent.name);
 // Log the child's age
-console.log(parent.child.name);
+  console.log(parent.child.name);
 // Log the name and age of the grandchild
-console.log(parent.child.grandchild.name)
-console.log(parent.child.grandchild.age)
+  console.log(parent.child.grandchild.name)
+  console.log(parent.child.grandchild.age)
 // Have the parent speak
   console.log(parent.speak());
 // Have the child speak
-console.log(parent.child.speak());
+  console.log(parent.child.speak());
 // Have the grandchild speak
-console.log(parent.child.grandchild.speak())
+  console.log(parent.child.grandchild.speak())
